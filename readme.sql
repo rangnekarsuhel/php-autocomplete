613798620a24c8648e240bb71ab1e89d037eb721
rangnekarsuhel <rangnekar.suhel@gamil.com>
2012-06-12 23:28:11 +0530

    Readme.sql updated again11.28

--- readme.sql
+++ readme.sql
@@ -1,3 +1,6 @@
 step :
 
-1) plz follow the autocomplete.html code structure. all js,css,etc included in html file
+1) 
+
+2) rthfbnfdntghmjgfmjyfgmjhfmj hgjm
+plz follow the autocomplete.html code structure. all js,css,etc included in html file
